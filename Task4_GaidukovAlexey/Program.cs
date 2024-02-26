6688950dcabaad582ca11cd5ac79d2996e5c018d
GaiDuck <alex.gaidukov@yandex.ru>
2024-02-26 03:04:51 +0300

    Добавьте файлы проекта.

---
+++ Task4_GaidukovAlexey/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_GaidukovAlexey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите часы:");
+            bool hoursParsed = int.TryParse(Console.ReadLine(), out int hours);
+            
+            Console.WriteLine("Введите минуты:");
+            bool minutesParsed = int.TryParse(Console.ReadLine(), out int minutes);
+
+            Console.WriteLine($"\nВы ввели время: {hours}:{minutes}");
+
+            ArrowsAngle(hours, minutes);
+        }
+
+
+        public static void ArrowsAngle(int hours, int minutes)
+        {
+            if (minutes > 59 || hours > 24 || minutes < 0 || hours < 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Введены некорректные данные.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadKey();
+            }
+            else
+            {
+                if (hours > 12)
+                {
+                    hours /= 12;
+                }
+
+                double minuteArrowAngle = minutes * 6;
+                double hourArrowAngle = hours * 30 + minutes * 0.5;
+                double ArrowsAngle = Math.Abs(hourArrowAngle - minuteArrowAngle);
+
+                if (ArrowsAngle > 180)
+                {
+                    ArrowsAngle = 360 - ArrowsAngle;
+                }
+
+                Console.WriteLine($"Угол между часовой и минутной стрелкой: {ArrowsAngle} градусов.");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Task4_GaidukovAlexey/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Task4_GaidukovAlexey")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task4_GaidukovAlexey")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("75b27c85-caa9-42e2-9e29-64d0011d7929")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
